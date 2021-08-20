d866b16d4c8d628a2d04949ac9fe962dad35204c
adtt123 <arshiadutta10@gmail.com>
2021-08-21 00:09:12 +0530

    Added and edited Resources Pages and added some styling to the error pages

--- app.py
+++ app.py
@@ -20,11 +20,49 @@ def home():
 def todo():
     return render_template('todo.html')
 
+@app.route('/resources', methods=['GET', 'POST'])
+def resources():
+    return render_template('resources.html')
+
+@app.route('/relaxation', methods=['GET', 'POST'])
+def relax():
+    return render_template('relax.html')
+
+@app.route('/ebooks', methods=['GET', 'POST'])
+def ebooks():
+    return render_template('ebooks.html')
+
+@app.route('/ebooks6', methods=['GET', 'POST'])
+def ebooks6():
+    return render_template('ebooks/ebooks6.html')
+
+@app.route('/ebooks7', methods=['GET', 'POST'])
+def ebooks7():
+    return render_template('ebooks/ebooks7.html')
+
+@app.route('/ebooks8', methods=['GET', 'POST'])
+def ebooks8():
+    return render_template('ebooks/ebooks8.html')
+
+@app.route('/images', methods=['GET', 'POST'])
+def images():
+    return render_template('imgs&diagrams.html')
+
+@app.route('/audio', methods=['GET', 'POST'])
+def audios():
+    return render_template('audios.html')
 
 @app.route('/img_pdf', methods=['GET', 'POST'])
 def img_pdf():
     return render_template('img_pdf.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
 
 @app.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
